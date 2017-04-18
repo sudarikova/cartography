bf9599d0d442540ee1b508dfdc4bcaa1dfa4a3a1
Jane Sudarikova <yo.marceline@gmail.com>
2017-04-19 01:36:42 +0300

    Delete mapafrica.R

--- mapafrica.R
+++
@@ -1,9 +0,0 @@
-
-x <- read.csv("nasals1.csv", sep = ";", header = TRUE, encoding = "UTF-8")
-x$popup <- aff.lang(x$language)
-map.feature(languages = x$language,
-            features = x$feature,
-            popup = x$popup,
-            control = TRUE, 
-            radius=7,
-            opacity = 0.7)
